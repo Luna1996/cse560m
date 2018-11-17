ce3f7f1b4817c9bb6e113b4148c54f3ad7b8a7fc
l+l l +K <yifu.w@wustl.edu>
2018-11-16 23:14:11 -0600

    hw3

--- hw3/run.py
+++ hw3/run.py
@@ -1,15 +1,13 @@
 import subprocess
-import linecache
 
 d1 = [256, 512, 1024, 2048, 4096]
 d2 = [4, 8, 16, 32, 64, 128, 256]
-ls = []
 
 savedir
 
 for a1 in d1:
   for a2 in d2:
-    for a3 in d3:
+    for a3 in d2:
       savedir = 'd%dx%dx%d'%(a1,a2,a3)
       print(savedir,'start')
       subprocess.check_call('$GEM5/build/ARM/gem5.opt',\
