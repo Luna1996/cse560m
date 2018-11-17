05cf0332c5f6af47c8bd7fc084a0c19116578763
l+l l +K <yifu.w@wustl.edu>
2018-11-16 22:54:00 -0600

    hw3

--- hw3/hw3opts.py
+++ hw3/hw3opts.py
@@ -5,8 +5,13 @@ from textwrap import TextWrapper
 #add options for number of ROB entries, IQ entries, and number of physical
 #floating point registers
 def addHW3Opts(parser):
+  parser.add_option("--num-rob-entries",type="int",default=192)
+  parser.add_option("--num-iq-entries",type="int",default=64)
+  parser.add_option("--num-phys-float-regs",type="int",default=256)
 
 #set parameters taken in from options on command line
 def set_config(cpu_list, options):
   for cpu in cpu_list:
-    # set parameters for each thing
+    cpu.numROBEntries = options.num_rob_entries
+    cpu.numIQEntries = options.num_iq_entries
+    cpu.numPhysFloatRegs = options.num_phys_float_entries
---
+++ hw3/run.py
@@ -0,0 +1,22 @@
+import subprocess
+import linecache
+
+d1 = [256, 512, 1024, 2048, 4096]
+d2 = [4, 8, 16, 32, 64, 128, 256]
+ls = []
+
+savedir
+
+for a1 in d1:
+  for a2 in d2:
+    for a3 in d3:
+      savedir = 'd%dx%dx%d'%(a1,a2,a3)
+      subprocess.check_call('$GEM5/build/ARM/gem5.opt',\
+      '--outdir="%s"'%(savedir)\
+      'hw3config.py',\
+      '-c','$GEM5/../test_progs/daxpy/daxpy_arm_big',\
+      '--cpu-type="DerivO3CPU"',\
+      '--caches','--l2cache',\
+      '--num-phys-float-regs=%d'%(a1),\
+      '--num-rob-entries=%d'%(a2),\
+      '--num-iq-entries=%d'%(a3))
