2cdc182a7832da7ee9a93b0aabb6cc8e5cd35d25
l+l l +K <yifu.w@wustl.edu>
2018-11-16 23:23:33 -0600

    hw3

--- hw3/run.py
+++ hw3/run.py
@@ -8,7 +8,7 @@ for a1 in d1:
     for a3 in d2:
       savedir = 'd%dx%dx%d'%(a1,a2,a3)
       print(savedir,'start')
-      subprocess.check_call('$GEM5/build/ARM/gem5.opt',\
+      subprocess.check_call(['$GEM5/build/ARM/gem5.opt',\
       '--outdir="%s"'%(savedir),\
       'hw3config.py',\
       '-c','$GEM5/../test_progs/daxpy/daxpy_arm_big',\
@@ -16,5 +16,5 @@ for a1 in d1:
       '--caches','--l2cache',\
       '--num-phys-float-regs=%d'%(a1),\
       '--num-rob-entries=%d'%(a2),\
-      '--num-iq-entries=%d'%(a3))
+      '--num-iq-entries=%d'%(a3)])
       print(savedir,'done')
