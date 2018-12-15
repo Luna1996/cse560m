026fc2ac7971a8dd0b47e4f89217e071be1ce6f0
l+l l +K <yifu.w@wustl.edu>
2018-12-14 19:38:25 -0600

    Update hw4opts.py

--- hw4/configs/hw4opts.py
+++ hw4/configs/hw4opts.py
@@ -3,22 +3,23 @@ def addHW4Opts(parser):
   parser.add_option("--pipeline_width", type="int", default=8)
   parser.add_option("--bp-type", type="str", default="LTAGE")
 
+
 # set parameters taken in from options on command line
 def set_config(cpu_list, options):
   for cpu in cpu_list:
-    if cpu.fetchWidth:
+    if hasattr(cpu, 'fetchWidth'):
       cpu.fetchWidth = options.pipeline_width
-    if cpu.decodeWidth:
+    if hasattr(cpu, 'decodeWidth'):
       cpu.decodeWidth = options.pipeline_width
-    if cpu.renameWidth:
+    if hasattr(cpu, 'renameWidth'):
       cpu.renameWidth = options.pipeline_width
-    if cpu.dispatchWidth:
+    if hasattr(cpu, 'dispatchWidth'):
       cpu.dispatchWidth = options.pipeline_width
-    if cpu.issueWidth:
+    if hasattr(cpu, 'issueWidth'):
       cpu.issueWidth = options.pipeline_width
-    if cpu.wbWidth:
+    if hasattr(cpu, 'wbWidth'):
       cpu.wbWidth = options.pipeline_width
-    if cpu.commitWidth:
+    if hasattr(cpu, 'commitWidth'):
       cpu.commitWidth = options.pipeline_width
-    if cpu.squashWidth:
+    if hasattr(cpu, 'squashWidth'):
       cpu.squashWidth = options.pipeline_width
